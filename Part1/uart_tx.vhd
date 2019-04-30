8a63ebd17f0077c5ab3bf257b8ae9578d32ce2a9
raulmori <47406865+raulmori@users.noreply.github.com>
2019-04-30 03:08:21 -0400

    Update uart_tx.vhd

--- Part1/uart_tx.vhd
+++ Part1/uart_tx.vhd
@@ -1,6 +1,8 @@
 
 --We use the SENDING-UART as reference to buid this RECEIVING-UART here
 
+--This code does not have a "SEND" in the ARCHITECTURE
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -19,7 +21,7 @@ entity uart_tx is
                    en : in STD_LOGIC;
                    send : in STD_LOGIC;
                    rst : in STD_LOGIC;
-                   char : in STD_LOGIC_VECTOR (7 downto 0);
+                   char : in STD_LOGIC_VECTOR (7 downto 0);      --This is the "DATA" that will hold our NETID 
                    ready : out STD_LOGIC;
                    tx : out STD_LOGIC
                  );
@@ -31,43 +33,46 @@ architecture Behavioral of uart_tx is
         type state is (idle, start, data);      --These are the TEMPORARY "TYPES" for the Finite-State-Machine's "STATES"
         signal curr : state := idle;        --This is just another way of making. TEMPORARY signal of the Current "STATE" of the initial "STATE" "IDLE" 
         
-        signal D :std_logic_vector(7 downto 0) := X"00";
+        signal D :std_logic_vector(7 downto 0) := X"00";      --Here we start the 8-bit REGISTER at "0"
 
 
         begin               --This starts the part  that has to do with the receiving side Of the Finite State Machine (FSM)
+                ----------------------------------------------------------------------
                 FSM:process(clk)
                 variable count : natural := 0;
                         begin
-                            if rising_edge(clk) then    --.This is the Main "IF" . This is if "reset" is high
-                                    if rst = '1' then
-                                            D <= X"00";
-                                            curr <= idle;
+                            if rising_edge(clk) then    --.This is the Main "IF" .
+                                    if rst = '1' then                  --This is if "RESET" BUTTON was Pressed
+                                            D <= X"00";                   --This is the D-FlipFlop REGISTER being Cleared
+                                            curr <= idle;             --It goes back to the "IDLE" "STATE"
                                     end if;
                                     
                                     if en = '1' then           --This is the "SUB-IF" because the main-if wasn't closed
                                             case curr is                    --Here we start a "CASE"
                                                     when idle => 
-                                                            ready <= '1';
-                                                            tx <= '1';
-                                                            if send = '1' then
-                                                                curr <= start;
+                                                            ready <= '1';             --Remember that "READY" is only "1" When we are in the "IDLE" "STATE"
+                                                            tx <= '1';              
+                                                            if 
+                                                              = '1' then
+                                                                curr <= start;        --This sets the Current "STATE" to the  "STATE" of "START"
                                                             end if;
                                                     when start => 
                                                             ready <='0';
                                                             tx <= '0';
-                                                            D <= char;
+                                                            D <= char;            --This store "CHAR" into  "D-FlipFLop" (REGISTER)
                                                             count := 0;
-                                                            curr <= data;
+                                                            curr <= data;             --This sets the Current "STATE" to the  "STATE" of "DATA"
                                                     when data =>
                                                             if count < 8 then
                                                                     tx <= D(count);
                                                                             count := count +1;
                                                             else
-                                                                tx <= '1';
-                                                                curr <= idle;
+                                                                tx <= '1';      
+                                                                curr <= idle;        --This sets the Current "STATE" to the Initial "STATE" of "IDLE"
                                                             end if;
                                             end case;                       
                                     end if;                 --Here we end the second "SUB-IF"
                             end if;                     --Here we end the "MAIN-IF"
                 end process;
+                ----------------------------------------------------------------------              
 end Behavioral;
