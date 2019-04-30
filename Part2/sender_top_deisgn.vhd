cdb91249db5ebdcfefe3fd10f69a1f84e154fa25
raulmori <47406865+raulmori@users.noreply.github.com>
2019-04-30 06:28:17 -0400

    Update sender_top_deisgn.vhd

--- Part2/sender_top_deisgn.vhd
+++ Part2/sender_top_deisgn.vhd
@@ -1,3 +1,7 @@
+--Remember that to make the PORT connections easier, we can visualize the Design using the RTL given to us
+--Notice we don't use a TEMPORARY variable for "CLOCK" when connecting "PORTS"
+--When Connecting PORTS we go from Right to Left
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -14,7 +18,7 @@ entity sender_top is
         Port (
                    TXD : in STD_LOGIC;
                    btn : in STD_LOGIC_VECTOR (1 downto 0);
-                   clk : in STD_LOGIC;
+                   clk : in STD_LOGIC;                                  --This is the "CLOCK"
                    RXD : out STD_LOGIC;
                    CTS : out STD_LOGIC;
                    RTS : out STD_LOGIC);
@@ -66,37 +70,46 @@ architecture Structural of sender_top is
 --------------------------------------------------------------------------       
 
     
-        begin                           --We start the PORT-MAPS to connect ENTITY-COMPONENT with MAIN-TEMPORARY signals
-                
+        begin                               
                 rts <= '0';                     --IMPORANT "RTS" has to be Grounded
                 cts <= '0';                     --IMPORANT "CTS" has to be Grounded
                 
-                clkdiv: clk_div
-                    port map(clk=> clk,
-                             div =>div);
-                rstdbnc: debounce
-                    port map(clk => clk,
-                             btn => btn(0),
-                             dbnc => rstbtn);
-                btndbnc:debounce
-                    port map(clk => clk,
-                             btn => btn(1),
-                             dbnc => btn1);
-                snder: sender
-                    port map(clk => clk,
-                             btn => btn1,
-                             en => div,
-                             ready => ready,
-                             rst => rstbtn,
-                             send => snd,
-                             char => char);
-                u5: uart
-                    port map(clk => clk,
-                             en => div,
-                             send => snd,
-                             rx => TXD,
-                             rst => rstbtn,
-                             charSend => char,
-                             ready => ready, 
-                             tx => RXD);
+
+                clkdiv: clk_div port map(
+                                     clk=> clk,         --Here we connect the MAIN-CLOCK to the "Clock-Divider"
+                                     div =>div          --Here we connect the OUTPUT of the "Clock-Divider" to a TEMPORARY Signal
+                                        );
+                              
+                rstdbnc: debounce port map(
+                                     clk => clk,        --Here we connect the MAIN-CLOCK to one of the "BUTTONS"
+                                     btn => btn(0),     --Here we connect one of the MAIN Bit-Button to the INPUT of one of our "BUTTONS"
+                                     dbnc => rstbtn             --Here we convert the OUTPUT of one of our "BUTTON"S into a TEMPORARY signal "RSTBTN"
+                                          );
+                        
+                btndbnc:debounce port map(
+                                     clk => clk,        --Here we connect the MAIN-CLOCK to one of the "BUTTONS"       
+                                     btn => btn(1),
+                                     dbnc => btn1.              --Here we convert the OUTPUT of one of our "BUTTON"S into a TEMPORARY signal "BTN1"
+                                        );
+                
+                snder: sender port map(
+                                     clk => clk,         --Here we connect the MAIN-CLOCK to  the "SENDER"
+                                     btn => btn1,          --Here TEMPORARY Signal "BTN1" is connected to "BTN" of "SENDER"
+                                     en => div,            --Here TEMPORARY Signal "DIV" is connected to "ENABLE" of "SENDER"
+                                     ready => ready,
+                                     rst => rstbtn,       --Here TEMPORARY Signal "RSTBTN" is connected to "RESET" of "SENDER"
+                                     send => snd,         --Here the OUTPUT of "SENDER" called "SEND"  connected to TEMPORARY signal "SND"
+                                     char => char
+                                     );
+                         
+                u5: uart port map.  (
+                                     clk => clk,         --Here we connect the MAIN-CLOCK to  the "UART"
+                                     en => div,         --Here we connect the TEMPORARY signal for the "Clock_Divider" OUTPUT and connect it to the "ENABLE" of the "UART"
+                                     send => snd,        --Here the TEMPORARY Signal "SND" is connected to the INPUT of the "UART" called "SEND"
+                                     rx => TXD,
+                                     rst => rstbtn,
+                                     charSend => char,
+                                     ready => ready, 
+                                     tx => RXD
+                                    );
 end Structural;
